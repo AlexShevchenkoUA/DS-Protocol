08981bc8ca42b036bba01532fe0a9243bffdfefe
AlexShevchenko <alex.shevchenko.ua@gmail.com>
2019-03-21 19:52:59 +0200

    Add hash runtime benchmark

--- src/main/java/ua/training/App.java
+++ src/main/java/ua/training/App.java
@@ -10,61 +10,15 @@ import ua.training.hash.SimpleHashFunction;
 
 import java.io.FileInputStream;
 import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.Writer;
 import java.math.BigInteger;
-import java.util.ArrayList;
-import java.util.List;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Random;
 
 
 public class App 
 {
-    public static void main( String[] args ) throws Exception {
-        Random random = new Random();
-
-        System.out.println(Long.toUnsignedString(new SimpleMistyCipher(1234L).encrypt(1234L), 16));
-        System.out.println(new MistyCipher(BigInteger.valueOf(1234L)).encrypt(BigInteger.valueOf(1234L)).toString(16));
-
-        /*ElGamalSignatureManager digitalSignature = new ElGamalSignatureManager();
-
-        digitalSignature.setQ(new BigInteger("57A9144B382BFF0E5C25C9288DF44D23", 16));
-        digitalSignature.setA(new BigInteger("9E93A4096E5416CED0242228014B67B5", 16));
-
-
-        long message = random.nextLong();
-
-        digitalSignature.setRandom(random);
-
-        BigInteger x = new BigInteger(digitalSignature.getP().bitLength(), random).mod(digitalSignature.getP());
-        BigInteger y = digitalSignature.getA().modPow(x, digitalSignature.getP());
-
-        Signature signature = digitalSignature.signature(message, x);
-
-        System.out.println(digitalSignature.verify(signature, message, y));*/
-
-        //
-
-        /*SimpleHashFunction hashFunction = new MerkleDamgardSimpleHashFunction(new SimpleMistyCipher());
-
-        InputStream stream = new FileInputStream("1.txt");
-
-        List<Byte> bytes = new ArrayList<>();
-
-        int b = stream.read();
-
-        while (b != -1) {
-            bytes.add((byte) b);
-            b = stream.read();
-
-        }
-
-        byte[] bytes_array = new byte[bytes.size()];
-
-        for (int i = 0; i < bytes.size(); i++) {
-            bytes_array[i] = bytes.get(i).byteValue();
-        }
-
-        stream.close();
-
-        System.out.println(Long.toUnsignedString(hashFunction.hash(bytes_array), 16));*/
-    }
+    public static void main( String[] args ) throws Exception { }
 }
---
+++ src/test/java/ua/training/BenchmarkTest.java
@@ -0,0 +1,60 @@
+package ua.training;
+
+import org.junit.Before;
+import org.junit.Test;
+import ua.training.cipher.SimpleMistyCipher;
+import ua.training.hash.MerkleDamgardSimpleHashFunction;
+import ua.training.hash.SimpleHashFunction;
+
+import java.io.*;
+import java.nio.file.Paths;
+import java.util.Random;
+
+import static java.lang.Long.toUnsignedString;
+import static java.lang.String.valueOf;
+import static java.lang.System.currentTimeMillis;
+import static java.nio.file.Paths.get;
+
+public class BenchmarkTest {
+    private SimpleHashFunction hashFunction;
+
+    private long size = 64 * (1L << 30) + 1L;    //Size 64 GB
+    private String tempFile = "temp.bin";
+
+    @Before
+    void initHashFunction() {
+        hashFunction = new MerkleDamgardSimpleHashFunction(new SimpleMistyCipher());
+    }
+
+    @Before
+    void createTempFile() throws IOException {
+        Random random = new Random();
+        byte[] bytes = new byte[128];
+        long time = currentTimeMillis();
+
+        try (OutputStream stream = new BufferedOutputStream(new FileOutputStream(valueOf(get(tempFile))))) {
+            for (long i = 0; i < size; i++) {
+                random.nextBytes(bytes);
+                stream.write(bytes);
+            }
+        }
+
+        System.out.println("Writing file time: " + (currentTimeMillis() - time));
+    }
+
+    @Test
+    void benchmark() throws IOException {
+        for (int i = 5; i < 10; i++) {
+            template((1 << i) * (1 << 20));
+        }
+    }
+
+    void template(int batchSize) throws IOException {
+        try (InputStream stream = new BufferedInputStream(new FileInputStream(valueOf(get(tempFile))), batchSize)) {
+            long time = currentTimeMillis();
+            long hash = hashFunction.hash(stream);
+            System.out.println("Batch size - " + batchSize + " hash - " + toUnsignedString(hash, 16) + " time - " + (time = currentTimeMillis() - time));
+            System.out.println("Speed - " + (((double) (size >>> 20)) / time) + " MB/ms");
+        }
+    }
+}
